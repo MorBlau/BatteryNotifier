d7d6fd8818d3e9d3ea0db5e3d89e8c32f34dfbe4
mor <mor.blau@trackimo.com>
2019-04-08 08:54:08 +0300

    version name in about activity

--- BatteryNotifier/src/main/java/com/mblau/batterynotifier/AboutActivity.kt
+++ BatteryNotifier/src/main/java/com/mblau/batterynotifier/AboutActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_about.*
+import kotlinx.android.synthetic.main.content_about.*
 
 class AboutActivity : AppCompatActivity() {
 
@@ -11,7 +12,13 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
         setSupportActionBar(toolbar)
+        setVersionName()
+    }
 
+    private fun setVersionName() {
+        val versionName = applicationContext.packageManager.getPackageInfo(packageName, 0).versionName
+        val aboutVersionText = this.getString(R.string.about_version) + " " + versionName
+        aboutVersion.text = aboutVersionText
     }
 
 }
